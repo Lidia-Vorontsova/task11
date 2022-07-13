0b9d5cd9672ded819fd2b6a0bf0258b408de6605
Lidia <lidiya_vorontsov@mail.ru>
2022-07-13 16:55:33 +0300

    task 11

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100, 999);
+Console.WriteLine($"Наше случайное число = {number}");
+
+int numberSecond = number / 100;
+int numberThree = number % 10;
+int result = numberSecond * 10 + numberThree;
+Console.WriteLine($"Наше случайное число ={number},исключая среднюю цифру ={result}");
